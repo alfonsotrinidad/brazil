c0c2467cb72a133a08661bc52f785d1a2e3d5565
alfonsotrinidad <43577249+alfonsotrinidad@users.noreply.github.com>
2020-10-09 07:24:28 -0500

    validacion formulario usuarios

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -18,9 +18,17 @@ export class UsuariosComponent implements OnInit {
   
   createFormGroup(){
     return new FormGroup({
-      nombre : new FormControl("",[Validators.required,Validators.minLength(5)]),
-      email  : new FormControl("",[Validators.required,Validators.minLength(5),
-         	Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+      id       : new FormControl("",[Validators.required,Validators.minLength(5)]),  
+      nombre   : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      email    : new FormControl("",[Validators.required,Validators.minLength(5),	Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      cpf      : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      phone    : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      password : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      photourl : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      location : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      points   : new FormControl("",[Validators.required,Validators.minLength(5)]),
+      settings : new FormControl("",[Validators.required,Validators.minLength(5)])
+   
     });
   }
 
@@ -43,8 +51,15 @@ limpiar(){
   this.formulario.reset();
 }
 
-
+get id(){return this.formulario.get('id') }
 get nombre(){return this.formulario.get('nombre') }
 get email(){return this.formulario.get('email') }
+get cpf(){return this.formulario.get('cpf') }
+get phone(){return this.formulario.get('phone') }
+get password(){return this.formulario.get('password') }
+get photourl(){return this.formulario.get('photourl') }
+get location(){return this.formulario.get('location') }
+get points(){return this.formulario.get('points') }
+get settings(){return this.formulario.get('settings') }
 
 }
